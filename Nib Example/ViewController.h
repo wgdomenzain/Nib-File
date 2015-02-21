edd1cc24c2b68ced8a357d3ac5c12eaa04f32805
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-02-21 09:41:43 -0600

    Initial Commit

---
+++ Nib Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nib Example
+//
+//  Created by Walter Gonzalez Domenzain on 21/02/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
